235d98e7be27369a5343fc14cd3672823a53d32f
99 Apps <99appscomp@gmail.com>
2026-05-29 20:16:47 +0300

    firebase setup complated for android

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
  * @format
  */
 
+import '@react-native-firebase/app';
+import crashlytics from '@react-native-firebase/crashlytics';
 import { AppRegistry } from 'react-native';
+
+crashlytics().log('App initialized');
 import App from './App';
 import { name as appName } from './app.json';
 
